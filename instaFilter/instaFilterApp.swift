a9652974895095c4b4110423e032ff560fffe639
ganudoomer <ganuiscool@gmail.com>
2021-12-11 13:09:44 +0530

    Initial Commit

---
+++ instaFilter/instaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  instaFilterApp.swift
+//  instaFilter
+//
+//  Created by Sree on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct instaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
